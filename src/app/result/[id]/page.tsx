28ec456b885b87a1d850320d70546b62c5c3460f
weissleon <weissleon1943@gmail.com>
2023-02-23 17:54:34 +0900

    Add address

--- src/app/result/[id]/page.tsx
+++ src/app/result/[id]/page.tsx
@@ -17,6 +17,7 @@ export default async function Result({ params }: Props) {
       <h1>결과</h1>
       <p>{`이름: ${data.name}`}</p>
       <p>{`좌표: ${data.coordinate}`}</p>
+      <p>{`주소: ${data.address}`}</p>
       <p>{`거리: ${data.distance}미터`}</p>
       <p>{`시간: ${data.time / 60}분`}</p>
 
